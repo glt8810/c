da02a12d772f6227c150f54464e95d61064a63e0
Gabriel Leal Togni <glt8810@moval.edu>
2017-06-20 03:26:02 -0500

    Create queue.c

---
+++ PatientQueue/queue.c
@@ -0,0 +1,4 @@
+Program created in C for one of my classes.
+This program is a queue where the user can add patients to a list. There can only be 14 patients per day and each patient takes up
+30 minutes. If the user wants to see a patient and delete it, there is an option for that. The user can also review which patients
+are left for the day, or start a new day.
